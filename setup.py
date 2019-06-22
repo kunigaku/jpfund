8f144dd01efe7e774eec2c3c35ef16b1b2a1cb64
kunigaku <kunigaku@gmail.com>
2019-06-23 00:38:10 +0900

    long_description_content_type="text/markdown",

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ assert author
 assert author_email
 assert url
 
-with open('README.rst', encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 
@@ -58,6 +58,7 @@ setup(
 
     description='Read prices of funds in Japan.',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords='pandas, fund',
 
     classifiers=[
